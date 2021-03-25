7578913ddd2c3f7fdc350e430db315bb8a3745fc
SaymonLevi <semion118@gmail.com>
2021-03-25 04:34:15 +0200

    logs are working

--- Utils/MinorTest.py
+++ Utils/MinorTest.py
@@ -4,7 +4,7 @@ import subprocess
 import logging 
 from multiprocessing import Process
 
-LOG_PATH = "SYMP/Storage/Logs"
+LOG_PATH = "./Storage/Logs"
 
 class MinorTest:
     def __init__ (self, test_name, parameters=None):
@@ -46,6 +46,9 @@ class MinorTest:
         self.logs(result_array)
 
     def logs(self,test_result_array):
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+
         for i in range (0,len(test_result_array)):
             logging.basicConfig(format='[INFO] : %(asctime)s - %(message)s', level = logging.INFO, filename = self.log_path, filemode='w') 
             logging.info(test_result_array[i])
